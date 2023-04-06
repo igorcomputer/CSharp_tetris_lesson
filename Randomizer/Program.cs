b8d0d75fcd428b4c29c7252bed04e14faef64cae
igorcomputer <igorcomputer@mail.ru>
2023-04-07 00:49:53 +0300

    Add Randomizer dir

---
+++ Randomizer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Randomizer {
+    class Program {
+        static void Main(string[] args) {
+            Random rand = new Random();
+            
+            for (int i = 0; i < 20; i++) {
+                int x = rand.Next(0, 5);
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
