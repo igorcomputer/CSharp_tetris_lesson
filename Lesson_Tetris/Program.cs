00bd5902767eef0529f5bdbb3810a1245d64b1e9
igorcomputer <igorcomputer@mail.ru>
2023-04-07 12:55:50 +0300

    Add FigureGenerator

--- Lesson_Tetris/Program.cs
+++ Lesson_Tetris/Program.cs
@@ -8,41 +8,21 @@ namespace Lesson_Tetris {
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            Figure s = new Stick(20, 5, '*');
-            s.Draw();
+            FigureGenerator generator = new FigureGenerator(20, 0, '*');
+            Figure s;
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Rotate();
-            s.Draw();
+            while (true) {
+                s = generator.GetNewFigure();
+                s.Draw();
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Rotate();
-            s.Draw();
+                for (int i = 0; i < 15; i++) {
+                    s.Hide();
+                    s.Move(Direction.DOWN);
+                    s.Draw();
+                    Thread.Sleep(200);
+                }
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.LEFT);
-            s.Draw();
-
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.DOWN);
-            s.Draw();
-
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.RIGHT);
-            s.Draw();
-
-            //Figure[] f = new Figure[2];
-            //f[0] = new Square(2, 5, '*');
-            //f[1] = new Stick(6, 6, '*');
-
-            //foreach(Figure fig in f) {
-            //    fig.Draw();
-            //}
+            }
 
             Console.ReadLine();
 
