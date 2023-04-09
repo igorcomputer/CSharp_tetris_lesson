d1f2c5203e27fc8ecc27214c933787c55548ffce
igorcomputer <igorcomputer@mail.ru>
2023-04-09 23:01:59 +0300

    Refactor class Field

--- Lesson_Tetris/Field.cs
+++ Lesson_Tetris/Field.cs
@@ -14,8 +14,8 @@ namespace Lesson_Tetris {
             }
             set {
                 _width = value;
-                Console.SetWindowSize(_width, _height);
-                Console.SetBufferSize(_width, _height);
+                Console.SetWindowSize(value, Field.Height);
+                Console.SetBufferSize(value, Field.Height);
             }
         }
 
@@ -25,8 +25,8 @@ namespace Lesson_Tetris {
             }
             set {
                 _height = value;
-                Console.SetWindowSize(_width, _height);
-                Console.SetBufferSize(_width, _height);
+                Console.SetWindowSize(Field.Width, value);
+                Console.SetBufferSize(Field.Width, value);
             }
         }
 
