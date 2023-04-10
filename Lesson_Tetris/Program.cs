5e455800637eca7891a5877bfe6559f0858a739c
igorcomputer <igorcomputer@mail.ru>
2023-04-11 00:48:02 +0300

    Timer Off for Game over

--- Lesson_Tetris/Program.cs
+++ Lesson_Tetris/Program.cs
@@ -42,6 +42,7 @@ namespace Lesson_Tetris
                 if (currentFigure.IsOnTop())
                 {
                     WriteGameOver();
+                    timer.Elapsed -= OnTimerEvent;
                     return true;
                 } 
                 else
