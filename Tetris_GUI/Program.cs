8f856201ad5f7cff7b73b483764a3817d157c8c6
igorcomputer <igorcomputer@mail.ru>
2023-04-13 00:24:44 +0300

    Test SmallBasic

---
+++ Tetris_GUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace Tetris_GUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Turtle.Show();
+            //Turtle.Move(100);
+            //Turtle.TurnLeft();
+            //Turtle.Move(100);
+            //Turtle.Speed++;
+            //Turtle.Move(100);
+
+            GraphicsWindow.Width = 100;
+            GraphicsWindow.Height = 100;
+            GraphicsWindow.BackgroundColor = "LightBlue";
+            GraphicsWindow.PenColor = "red";
+            GraphicsWindow.DrawRectangle(20, 20, 10, 10);
+        }
+    }
+}
